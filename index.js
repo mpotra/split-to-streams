e26fc2a37712a43bd18e88cf83ccec9d5f531de1
Mihai Potra <mike@mpotra.com>
2017-11-03 17:26:50 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const StreamSplit = module.exports = require('./lib/stream');
+
+module.exports.StreamSplit = StreamSplit;
+
+module.exports.SimpleSplit = require('./lib/simple');
+
+module.exports.LineSplit = require('./lib/line');
